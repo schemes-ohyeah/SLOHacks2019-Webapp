6965801d6ab4596fa483ea1640f23fce6e16108e
Peter Yang <me@peteryang.io>
2019-02-02 11:42:51 -0800

    Updates

---
+++ precache-manifest.ea501e2105c17587cc833e4963d1da44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "428942e9d800ef6d0b22",
+    "url": "/static/css/main.017bdfe7.chunk.css"
+  },
+  {
+    "revision": "428942e9d800ef6d0b22",
+    "url": "/static/js/main.428942e9.chunk.js"
+  },
+  {
+    "revision": "fe779466ec42522fcfee",
+    "url": "/static/js/1.fe779466.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "66afb6fe33e92b7e950608a10a59e1b6",
+    "url": "/index.html"
+  }
+];
