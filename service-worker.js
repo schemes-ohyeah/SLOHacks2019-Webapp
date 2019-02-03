7e853c4ae2071493d4f7f12a825839b783a9b87b
Peter Yang <me@peteryang.io>
2019-02-02 20:36:39 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea501e2105c17587cc833e4963d1da44.js"
+  "/precache-manifest.5227eb712700af838326c58966a67750.js"
 );
 
 workbox.clientsClaim();
